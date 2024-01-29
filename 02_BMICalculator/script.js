e6fa370e5371237bc3245830d7817180053abed1
RishabhDhawad <rishabhdhawad29@gmail.com>
2024-01-30 00:06:34 +0530

    updated: bmi conditions

--- 02_BMICalculator/script.js
+++ 02_BMICalculator/script.js
@@ -18,5 +18,13 @@ form.addEventListener('submit', function(e) {
         const bmi = (weight / ((height * height)/10000)).toFixed(2)
         // show the result
         results.innerHTML = `<span>${bmi}</span>`;
+
+        if (bmi < 18.6) {
+            results.innerHTML = `Under Weight: <span>${bmi}</span>`
+        } else if ( bmi >= 18.6 &&  bmi < 24.9 ){
+            results.innerHTML = `Normal Wright: <span>${bmi}</span>`
+        } else {
+            results.innerHTML = `Over Weight: <span>${bmi}<span>`
+        }
     }
 })
